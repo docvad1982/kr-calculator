7ab2573a2ce5c28aa7fd4b029bedd5f500663bdd
docvad1982 <justlostspace@gmail.com>
2026-04-11 22:00:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,42 @@
-const CACHE_NAME = 'kr-calc-v1';
-const ASSETS = ['./calculator.html', './manifest.json', './icon-512.png'];
+const CACHE_NAME = 'kr-calc-v2';
+const ASSETS = [
+  './calculator.html',
+  './manifest.json',
+  './icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэш открыт');
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
